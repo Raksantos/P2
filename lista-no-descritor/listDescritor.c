dac6a61936882a0f4c0274b8616231b5e43dbc19
Rodrigo Santos da Silva <rodrigosnt3@gmail.com>
2019-11-07 20:45:19 -0300

    adicionando algumas funçoes para inserçao de elementos na lista

--- lista-no-descritor/listDescritor.c
+++ lista-no-descritor/listDescritor.c
@@ -34,7 +34,7 @@ List* init_list()
     return li;
 }
 
-void free_list(List *li)
+void free_list(List* li)
 {
     if(li != NULL){
         Element *no;
@@ -47,9 +47,43 @@ void free_list(List *li)
     }
 }
 
-int size_list(List *li)
+int size_list(List* li)//Essa função é desnecessária nesse cenário
 {
     if(li == NULL) return 0;
 
     return li->size;
+}
+
+int add_list_beginning(List* li, struct student st)
+{
+    if(li == NULL) return 0;
+    Element *no = (Element*) malloc(sizeof(List));
+
+    if(no == NULL) return 0;
+    no->data = st;
+    no->next = li->begin;
+    if(li->begin == NULL){
+        li->end = no;
+    }
+    li->begin = no;
+    li->size++;
+    return 1;
+}
+
+int add_list_end(List* li, struct student st)
+{
+    if(li == NULL) return 0;
+    Element *no = (Element*) malloc(sizeof(Element));
+
+    if(no == NULL) return 0;
+    no->data = st;
+    no->next = NULL;
+    if(li->begin == NULL){
+        li->begin = no;
+    }else{
+        li->end->next = no;
+    }
+    li->end = no;
+    li->size++;
+    return 1;
 }
--- lista-no-descritor/listDescritor.h
+++ lista-no-descritor/listDescritor.h
@@ -8,6 +8,10 @@ typedef struct description List;
 
 List* init_list();
 
-void free_list();
+void free_list(List* li);
 
-int size_list(List *li);
+int size_list(List* li);
+
+int add_list_end(List* li, struct student st);
+
+int add_list_beginning(List* li, struct student st);
