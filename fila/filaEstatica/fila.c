c4823393ceeefc5cb02b5c800238baf01190fe99
Rodrigo Santos da Silva <rodrigosnt3@gmail.com>
2019-12-01 18:15:01 -0300

    adicionando funçoes de adicionar, remover e consulta na fila

--- fila/filaEstatica/fila.c
+++ fila/filaEstatica/fila.c
@@ -53,3 +53,35 @@ int empty_queue(Queue* fi)
 
     return fi->size == 0 ? 1 : 0;
 }
+
+int add_queue(Queue* fi, struct student st)
+{
+    if(fi == NULL) return 0;
+
+    if(full_queue(fi)) return 0;
+
+    fi->data[fi->end] = st;
+    fi->end = (fi->end+1)%MAX;
+    fi->size++;
+
+    return 1;
+}
+
+int remove_queue(Queue* fi)
+{
+    if(fi == NULL || empty_queue(fi)) return 0;
+
+    fi->begin = (fi->begin+1)%MAX;
+    fi->size--;
+
+    return 1;
+}
+
+int select_queue(Queue* fi, struct student *st)
+{
+    if(fi == NULL || empty_queue(fi)) return 0;
+
+    *st = fi->data[fi->begin];
+
+    return 1;
+}
--- fila/filaEstatica/fila.h
+++ fila/filaEstatica/fila.h
@@ -9,3 +9,15 @@ typedef struct queue Queue;
 Queue* init_queue();
 
 void free_queue(Queue* fi);
+
+int size_queue(Queue* fi);
+
+int full_queue(Queue* fi);
+
+int empty_queue(Queue* fi);
+
+int add (Queue* fi, struct student al);
+
+int remove_queue(Queue* fi);
+
+int select_queue(Queue* fi, struct student *st);
