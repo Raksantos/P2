136166cf4ab2aed34953610b34dd1fdbb11eced4
Rodrigo Santos da Silva <rodrigosnt3@gmail.com>
2019-12-08 19:33:06 -0300

    iniciando hash table

--- hash-table/hash.c
+++ hash-table/hash.c
@@ -8,8 +8,45 @@ struct hash{
     struct student **itens;
 };
 
+typedef struct hash Hash;
+
 int main()
 {
-    QueuePrio *fp = init_queue();
+    Hash *ha = init_hash(1427);
     return 0;
+}
+
+Hash* init_hash(int table_size)
+{
+    Hash* ha = (Hash*) malloc(sizeof(Hash));
+
+    if(ha != NULL){
+        int i;
+        ha->table_size = table_size;
+        ha->itens = (struct student**) malloc(table_size * sizeof(struct student*));
+
+        if(ha->itens == NULL){
+            free(ha);
+            return NULL;
+        }
+        ha->qtd = 0;
+        for(i = 0; i < ha->table_size; i++){
+            ha->itens[i] = NULL;
+        }
+    }
+    return ha;
+}
+
+void free_hash(Hash* ha)
+{
+    if(ha != NULL){
+        int i;
+        for (i = 0; i < ha->table_size; i++){
+            if(ha->itens[i] != NULL){
+                free(ha->itens[i]);
+            }
+            free(ha->itens);
+            free(ha);
+        }
+    }
 }
--- hash-table/hash.h
+++ hash-table/hash.h
@@ -2,9 +2,7 @@ struct student{
     int registration;
     char name[30];
     double n1, n2, n3;
-}
-
-typedef struct hash Hash;
+};
 
 Hash* init_hash(int table_size);
 
@@ -16,6 +14,6 @@ int put_withoutColision(Hash* ha, struct student st);
 
 int get_withoutColision(Hash* ha, struct student* st);
 
-int put_enderOpened(Hash* ha, struct al);
+int put_enderOpened(Hash* ha, struct student* st);
 
 int get_enderOpened(Hash* ha, int registration, struct student* st);
