622a0beb7787481dba5ec8f33776f598ff227e5f
Rodrigo Santos da Silva <rodrigosnt3@gmail.com>
2019-12-13 20:28:51 -0300

    terminando lista de listas

--- exercicios/lista/maiorSequencia.c
+++ exercicios/lista/maiorSequencia.c
@@ -2,116 +2,121 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct element{
+struct element
+{
     int num;
     struct element *next;
 };
 
 typedef struct element Element;
 
-typedef struct element* List;
+typedef struct element *List;
 
-List* init_list()
+List *init_list()
 {
-    List *li = (List*) malloc(sizeof(List));//aloca espaço de memória pra lista
+    List *li = (List *)malloc(sizeof(List)); //aloca espaço de memória pra lista
 
-    if(li != NULL)//se tiver algum dado na lista
-        *li = NULL;//esvazia ela fazendo a atribuição
+    if (li != NULL) //se tiver algum dado na lista
+        *li = NULL; //esvazia ela fazendo a atribuição
 
-    return li;//retorna a lista vazia
+    return li; //retorna a lista vazia
 }
 
 int add_end(List *li, char num)
 {
-    if(li == NULL) return 0; // se a lista não existir, retorna erro
-    Element *no = (Element*) malloc(sizeof(Element)); //aloca espaço para o novo elemento
+    if (li == NULL)
+        return 0;                                     // se a lista não existir, retorna erro
+    Element *no = (Element *)malloc(sizeof(Element)); //aloca espaço para o novo elemento
 
-    if(no == NULL) return 0; //se ouver problemas na alocação, retorna 0
+    if (no == NULL)
+        return 0; //se ouver problemas na alocação, retorna 0
 
-    no->num = (int) num;//passa os dados
-    no->next = NULL;//faz com que o ponteiro aponte para null, já que é o último elemento
-    if((*li) == NULL){
-        *li = no;//se a lista for vazia ele vai inserir no início
+    no->num = (int)num; //passa os dados
+    no->next = NULL;    //faz com que o ponteiro aponte para null, já que é o último elemento
+    if ((*li) == NULL)
+    {
+        *li = no; //se a lista for vazia ele vai inserir no início
     }
     //se não for uma lista vazia
-    else{
-        Element *aux = *li;//cria um elemento auxiliar que irá pecorrer a lista, iniciando da cabeça
-        while(aux->next != NULL){//enquanto o ponteiro do elemento auxiliar não apontar para null, vai para o próximo
+    else
+    {
+        Element *aux = *li; //cria um elemento auxiliar que irá pecorrer a lista, iniciando da cabeça
+        while (aux->next != NULL)
+        {                    //enquanto o ponteiro do elemento auxiliar não apontar para null, vai para o próximo
             aux = aux->next; // pecorrendo a lista
         }
         aux->next = no; // aponta o último nó da lisa para o novo último nó
     }
 }
 
-void free_list(List* li)
+void free_list(List *li)
 {
-    if(li != NULL){//se a lista não estiver vazia
-        Element *no;//cria elemento auxiliar
-        while((*li) != NULL){//enquanto a lista não estiver vazia
-            no = *li;//auxilar recebe o valor do nó
-            *li = (*li)->next;//cabeça da lista apontara para o próximo elemento
-            free(no);//libera os dados do nó
+    if (li != NULL)
+    {                //se a lista não estiver vazia
+        Element *no; //cria elemento auxiliar
+        while ((*li) != NULL)
+        {                      //enquanto a lista não estiver vazia
+            no = *li;          //auxilar recebe o valor do nó
+            *li = (*li)->next; //cabeça da lista apontara para o próximo elemento
+            free(no);          //libera os dados do nó
         }
-        free(li);//ao acabar de pecorrer a lista, libera a memória da lista
+        free(li); //ao acabar de pecorrer a lista, libera a memória da lista
     }
 }
 
-int funcao_entrada(char *entrada)
+void verfica()
 {
-    fgets(entrada, 300, stdin);
-    getchar();
-    // scanf("%[^\n]s", entrada);
-    // printf("%s", entrada);
-    
-    int aux = strlen(entrada);
-    printf("%d\n", aux);
-    if(entrada[0] == '0' && aux == 2){
-        return 0;
-    }
-
-    return 1;
-}
-
-void verfica(){
-    char entrada[300];
+    char entrada[256];
     int i, j, begin = 0, end = 0, aux_begin = 0, aux_end = 0, diferenca_aux = 0, diferenca = 0;
 
-    if(funcao_entrada(entrada) != 0){
-        List *li = init_list();
-        for(i = 0; entrada[i] != '\n'; i++){
-            add_end(li, entrada[i]);
-        }
+    fgets(entrada, 256, stdin);
 
-        Element *no = *li;
+    int aux = strlen(entrada);
 
-        i = 0;
-        while(no != NULL){
-            if(no->num == 48){
-                aux_begin = i;
-                Element *aux = no;
-                for(j = i; aux->num != 49 && aux->next != NULL; j++){
-                    aux_end = j;
-                    aux = aux->next;
-                }
-                diferenca_aux = aux_end - aux_begin;
-                if(diferenca_aux > diferenca){
-                    diferenca = diferenca_aux;
-                    begin = aux_begin;
-                    end = aux_end;
-                }
-            }
-            i++;
-            no = no->next;
-        }
-
-        printf("%d %d\n", begin, end);
-        diferenca = 0;
-        begin = 0;
-        end = 0;
-
-        free_list(li);
-        verfica();
+    if (entrada[0] == '0' && aux == 1)
+    {
+        return;
     }
+
+    List *li = init_list();
+    for (i = 0; entrada[i] != '\n'; i++)
+    {
+        add_end(li, entrada[i]);
+    }
+
+    Element *no = *li;
+
+    i = 0;
+    while (no != NULL)
+    {
+        if (no->num == 48)
+        {
+            aux_begin = i;
+            Element *aux = no;
+            for (j = i; aux->num != 49 && aux->next != NULL; j++)
+            {
+                aux_end = j;
+                aux = aux->next;
+            }
+            diferenca_aux = aux_end - aux_begin;
+            if (diferenca_aux > diferenca)
+            {
+                diferenca = diferenca_aux;
+                begin = aux_begin;
+                end = aux_end;
+            }
+        }
+        i++;
+        no = no->next;
+    }
+
+    printf("%d %d\n", begin, end);
+    diferenca = 0;
+    begin = 0;
+    end = 0;
+
+    free_list(li);
+    verfica();
 }
 
 int main()
